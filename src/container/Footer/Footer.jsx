10288039523bfef6d7ff84861373845d8c58fcca
LOGIC_NOT_FOUND <74826591+ANSHJOSHI1811@users.noreply.github.com>
2023-06-05 09:53:05 +0530

    Intial Commit

---
+++ src/container/Footer/Footer.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+
+import { images } from '../../constants';
+import { AppWrap, MotionWrap } from '../../wrapper';
+import './Footer.scss';
+
+const Footer = () => {
+  return (
+    < >
+      <h2 className="head-text">Take a Tea & chat with me</h2>
+
+      <div className="app__footer-cards">
+        <div className="app__footer-card ">
+          <img src={images.email} alt="email" />
+          <a href="mailto:anshsjoshi@outlook.com" className="p-text">anshsjoshi@outlook.com</a>
+        </div>
+        <div className="app__footer-card">
+          <img src={images.mobile} alt="phone" />
+          <a href="tel:+91 6355025431" className="p-text">+91 6355025431</a>
+        </div>
+      </div>
+    { (
+        <div>
+         
+        </div>
+      )}
+    </>
+  );
+};
+
+export default AppWrap(
+  MotionWrap(Footer, 'app__footer'),
+  'contact',
+  'app__whitebg',
+);
