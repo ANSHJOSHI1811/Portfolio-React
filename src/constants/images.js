9526bdf8e260b4f67aa0fd7b179a38b664e7bd35
LOGIC_NOT_FOUND <74826591+ANSHJOSHI1811@users.noreply.github.com>
2023-06-06 10:52:44 +0530

    Final Commit1

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,30 +1,17 @@
-import email from '../assets/email-min.png';
 import mobile from '../assets/mobile-min.png';
-import api from '../assets/api-min.png';
 import cpp from '../assets/cpp-min.png';
 import css from '../assets/css-min.png';
-import figma from '../assets/figma-min.png';
-import flutter from '../assets/flutter-min.png';
 import git from '../assets/git-min.png';
-import graphql from '../assets/graphql-min.png';
 import html from '../assets/html-min.png';
 import javascript from '../assets/javascript-min.png';
-import mu5 from '../assets/mu5-min.png';
 import node from '../assets/node-min.png';
-import python from '../assets/python-min.png';
-import react from '../assets/react-min.png';
-import redux from '../assets/redux-min.png';
-import sass from '../assets/sass-min.png';
-import typescript from '../assets/typescrip-mint.png';
-import vue from '../assets/vue-min.png';
-
-
-
+import react from '../assets/react.png';
+import redux from '../assets/redux.png';
 import profile from '../assets/profile-min.png';
-import circle from '../assets/circle-min.svg';
+import circle from '../assets/circle.svg';
 import logo from '../assets/logo-min.png';
 import n from '../assets/n-min.png';
-import r from '../assets/r-min.png';
+import r from '../assets/r.png';
 import cultimate from '../assets/cultimate-min.png';
 import ihba from '../assets/ihba-min.png';
 import back from '../assets/back-min.png';
@@ -47,25 +34,22 @@ export default {
   n,
   r,
   cultimate,
-  email,
+
   mobile,
-  api,
+
   cpp,
   css,
-  figma,
-  flutter,
+ 
   git,
-  graphql,
+
   html,
   javascript,
-  mu5,
+ 
   node,
-  python,
+ 
   react,
   redux,
-  sass,
-  typescript,
-  vue,
+ 
  
   profile,
   circle,
